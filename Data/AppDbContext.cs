b3f7674465337950b600cd9384706bc19e8c8928
PedroMizani <pedromizani@outlook.com>
2023-05-20 16:16:00 -0300

    projeto api Balta.IO

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite(connectionString: "DataSource=app.db; Cache=Shared");
+        
+    }
+}
